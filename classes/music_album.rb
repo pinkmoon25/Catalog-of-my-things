9b64de8329ad09fbe20e874104bf6efc10693d00
usbbush <jeanbienvenusb@gmail.com>
2022-10-10 17:25:55 +0200

    Implement a new method in MusicAlbum Class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,2 +1,14 @@
-class MusicAlbum
+class MusicAlbum < Item
+  attr_reader :on_spotify
+
+  def initialize(id, publish_date, archived, on_spotify)
+    @on_spotify = on_spotify
+    super(id, publish_date, archived)
+  end
+
+  private
+
+  def can_be_archived?
+    @on_spotify && super
+  end
 end
