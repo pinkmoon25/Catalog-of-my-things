b8127dbbb433b977718ea97b24689b8d964fcc4a
Tushar <singhtushar348@gmail.com>
2022-10-11 14:21:34 +0530

    Add attribute accessor for public properties

--- classes/game.rb
+++ classes/game.rb
@@ -3,7 +3,7 @@ require_relative 'item'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
-  
+
   def initialize(publish_date, multiplayer, last_played_at)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
@@ -11,6 +11,7 @@ class Game < Item
   end
 
   private
+
   def can_be_archived?
     current_year = Date.today.year
     last_played_at_year = Date.parse(@last_played_at).year
