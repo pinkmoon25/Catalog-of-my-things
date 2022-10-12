f474df410e3c39a07acfd8b9c5d5aea0f5de7365
Tushar <singhtushar348@gmail.com>
2022-10-12 14:06:45 +0530

    Add author id when saving author details

--- classes/game_app.rb
+++ classes/game_app.rb
@@ -4,6 +4,9 @@ require_relative 'author'
 require_relative 'load_games'
 
 class GameApp
+  @game_file = './data/games.json'
+  @author_file = './data/authors.json'
+
   def self.create_author
     puts 'Enter Author details: '
     print 'First name: '
@@ -11,7 +14,7 @@ class GameApp
     print 'Last name: '
     last_name = gets.chomp
     new_author = Author.new(first_name, last_name)
-    save_author(first_name, last_name)
+    save_author(new_author)
     puts 'Author created successfully'
     new_author
   end
@@ -28,32 +31,34 @@ class GameApp
     puts 'Game created successfully'
     new_author = create_author
     new_game.add_author = new_author
-    save_game(published_date, multiplayer, last_played, new_game)
+    save_game(new_game)
   end
 
-  def self.save_game(published_date, multiplayer, last_played, game)
-    if File.size?('./data/games.json')
-      file = File.read('./data/games.json')
+  def self.save_game(game)
+    if File.size?(@game_file)
+      file = File.read(@game_file)
       games = JSON.parse(file)
-      games << { Publish_date: published_date, Multiplayer: multiplayer, Last_played: last_played,
+      games << { Publish_date: game.publish_date, Multiplayer: game.multiplayer,
+                 Last_played: game.last_played_at,
                  Author: "#{game.author.first_name} #{game.author.last_name}" }
-      File.write('./data/games.json', JSON.pretty_generate(games))
+      File.write(@game_file, JSON.pretty_generate(games))
     else
-      File.write('./data/games.json',
-                 JSON.pretty_generate([{ Publish_date: published_date, Multiplayer: multiplayer,
-                                         Last_played: last_played,
+      File.write(@game_file,
+                 JSON.pretty_generate([{ Publish_date: game.publish_date, Multiplayer: game.multiplayer,
+                                         Last_played: game.last_played_at,
                                          Author: "#{game.author.first_name} #{game.author.last_name}" }]))
     end
   end
 
-  def self.save_author(first_name, last_name)
-    if File.size?('./data/authors.json')
-      file = File.read('./data/authors.json')
+  def self.save_author(author)
+    if File.size?(@author_file)
+      file = File.read(@author_file)
       authors = JSON.parse(file)
-      authors << { Author: "#{first_name} #{last_name}" }
-      File.write('./data/authors.json', JSON.pretty_generate(authors))
+      authors << { Author: "#{author.first_name} #{author.last_name}", Id: author.id }
+      File.write(@author_file, JSON.pretty_generate(authors))
     else
-      File.write('./data/authors.json', JSON.pretty_generate([{ Author: "#{first_name} #{last_name}" }]))
+      File.write(@author_file, JSON.pretty_generate([{ Author: "#{author.first_name} #{author.last_name}",
+                                                       Id: author.id }]))
     end
   end
 
@@ -69,6 +74,6 @@ class GameApp
   def self.list_all_authors
     authors = Storage.load_authors
     puts 'No authors available yet, Please add authors' if authors.empty?
-    authors.each_with_index { |author, i| puts "#{i}) #{author['Author']}" }
+    authors.each_with_index { |author, i| puts "#{i}) #{author['Author']} Id: #{author['Id']}" }
   end
 end
