701b650675cd9dc918faca483c500c39481acb31
usbbush <jeanbienvenusb@gmail.com>
2022-10-12 11:45:28 +0200

    Add getter for archived property

--- classes/item.rb
+++ classes/item.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Item
-  attr_reader :genre, :author, :source, :label
+  attr_reader :genre, :author, :source, :label, :archived
   attr_accessor :publish_date
 
   def initialize(publish_date)
